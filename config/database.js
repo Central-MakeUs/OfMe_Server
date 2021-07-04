14dbc6d821006c1e23822b6bc82baf17750ad15f
ecvheo1 <ecvheo@naver.com>
2021-07-04 14:00:28 +0900

    first commit

--- config/database.js
+++ config/database.js
@@ -1,30 +1,17 @@
 const mysql = require('mysql2/promise');
 const {logger} = require('./winston');
-
-// TODO: 본인의 DB 계정 입력
-// const pool = mysql.createPool({
-//     host: '3.141.193.244',
-//     user: 'test',
-//     port: '3306',
-//     password: '980215',
-//     database: 'ryu'
-// });
+const database = require('../databasePool');
 
 const pool = mysql.createPool({
-        host :'localhost',
-        user:'root',
-        //port:,
-        password:'980215',
-        database:'Demo'
+    host: database.host,
+    user: database.user,
+    port: database.port,
+    password: database.password,
+    database: database.database
 });
 
-// const pool = mysql.createPool({
-//     host :'',
-//     user:'',
-//     port:'',
-//     password:'',
-//     database:''
-// });
+
+
 
 
 module.exports = {
